2ce596150127f7c1f735252ca2641552e158ce94
Mariel Van Norman <melvinopolis@gmail.com>
2016-01-19 22:31:15 -0500

    class exercises

---
+++ beer.js
@@ -0,0 +1,3 @@
+for (var i = 99; i > 0; i--) {
+	console.log(i + ' bottles of beer on the wall, ' + i + ' bottles of beer. Take one down, pass it around. ' + (i - 1) + ' bottles of beer on the wall.');
+}
---
+++ coffee.js
@@ -0,0 +1,11 @@
+// var coffeeConsumed = prompt('How many cups of coffee have you had today?');
+
+// coffeeConsumed = Number(coffeeConsumed);
+
+var coffeeConsumed = 2;
+
+if ( coffeeConsumed < 3 ) {
+	console.log("Yes, I'll take another cup of coffee!");
+} else {
+	console.log("I think I'm ok for now.");
+}
